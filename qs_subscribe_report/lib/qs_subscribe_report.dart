27e4b0351defddfdb1fd630ac089d4075edbb4e6
Song <791589545@qq.com>
2026-09-15 17:21:59 +0800

    2026.09.15
    添加忽略文件

--- qs_subscribe_report/lib/qs_subscribe_report.dart
+++ qs_subscribe_report/lib/qs_subscribe_report.dart
@@ -1,8 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:ip_location/ip_location.dart';
-import 'package:ip_location/ip_location_model.dart';
+import 'package:qs_ip_location/qs_ip_location.dart';
 import 'package:qs_asa_attribution_info/qs_asa_attribution_info.dart';
 import 'package:qs_aes_encrypt/qs_aes_encrypt.dart';
 import 'package:qs_device_info/qs_device_info.dart';
@@ -405,9 +404,9 @@ class QsSubscribeReport {
   }
 
   /// 根据 IP 获取省市区信息
-  static Future<IpLocationModel?> _getLocationByIp() async {
+  static Future<QsIpLocationModel?> _getLocationByIp() async {
     try {
-      IpLocationModel? location = await IpLocation.getIpLocation();
+      QsIpLocationModel? location = await QsIpLocation.getIpLocation();
       return location;
     } catch (e) {
       QsLog.error("获取位置信息失败: $e");
